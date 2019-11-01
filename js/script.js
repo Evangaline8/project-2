a39ecedb456690cf4baa1c7a814c4dbf26f7d8f0
fntcstudent <fntcstudent@fntcstudents-MacBook-Air.local>
2019-11-01 16:03:15 -0700

    put in images and changed some id names

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-console.log('hello world yvghytg')
+
+
+$("#selection").on("change", function(){
+ //console.log("hello");
+    const selected= $(this).val();
+    $.ajax ({
+        
+        method: "GET",
+        url:
+        "https://api.nytimes.com/svc/topstories/v2/" +selected+
+        ".json?api-key=shGq1U5v7Y9OnT5fmWGRNImpzizs7UgR",
+        dataType: "JSON"
+  })
+
+
+    .done(function(data){
+
+        const articleFilter = data.results.filter(function(filterSet) {
+            return filterSet.multimedia[4] !== undefined;
+
+        });
+        // console.log(articleFilter);
+    
+    const cutArticle = articleFilter.slice(0, 12);
+    
+    console.log(cutArticle);
+        $.each(cutArticle, function(index, object){
+         $("#articles").append(
+            `<article class="news_article" style="background:url(${object.multimedia[4].url})">
+                <p class="news_copy">
+                ${object.abstract}
+                </p>
+            </article>
+            `
+            
+            ); 
+
+        
+        });//close each 
+    
+    // ${object.multimedia[4]} ${object.abstract}
+    
+})
+    .fail(function(){
+        console.log("error");
+    });
+});
+
+
